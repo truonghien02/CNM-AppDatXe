48915bb487c407ce279e0767ad3cdea07dc3f2ce
Lê Thị Kim Hạnh <39234700+alicele134@users.noreply.github.com>
2018-11-14 10:00:02 +0700

    thao tác databse
    insert dữ liệu

--- App2/repos/clientRepos.js
+++ App2/repos/clientRepos.js
@@ -5,3 +5,15 @@ exports.loadAll = () => {
 	return db.load(sql);
 }
 
+exports.insertGeocode = (clientinfo) => {
+	console.log(clientinfo)
+	var sql = `update kh
+			 	set latitude='${clientinfo.latitude}', longtitude='${clientinfo.longtitude}'
+				where kh.name='${clientinfo.name}'
+					and kh.phone = '${clientinfo.phone}'
+					and kh.address = '${clientinfo.address}';
+			 `;
+		
+	return db.load(sql);
+}
+
